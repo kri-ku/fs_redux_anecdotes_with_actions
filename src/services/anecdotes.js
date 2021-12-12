550fce4d9463078e0d98f7825216a58ed555796e
kristiina kumila <kristiinakumila@gmail.com>
2021-12-12 11:37:01 +0200

    deploy app again

--- src/services/anecdotes.js
+++ src/services/anecdotes.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
 
-const baseUrl = 'http://localhost:3001/anecdotes'
+//BASE_URL https://sleepy-ocean-20338.herokuapp.com//anecdotes
+const baseUrl = process.env.BASE_URL || 'http://localhost:3001/anecdotes'
 
 const getAll = async () => {
   const response = await axios.get(baseUrl)
